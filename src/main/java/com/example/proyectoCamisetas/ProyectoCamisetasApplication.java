99f8c60768ca4ca2ff2916c8f4fea0f926913252
Sergio Trillo <sergoptrillorodriguez123@gmail.com>
2025-12-02 10:03:48 +0100

    Primer commit

---
+++ src/main/java/com/example/proyectoCamisetas/ProyectoCamisetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoCamisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCamisetasApplication.class, args);
+	}
+
+}
