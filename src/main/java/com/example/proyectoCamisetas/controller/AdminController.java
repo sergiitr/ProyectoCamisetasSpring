87c211189ded1bc48544821d0fcd7dc049de20df
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-12-05 12:42:34 +0100

    Refactor AdminController and add maestroDetalleCamisetas

--- src/main/java/com/example/proyectoCamisetas/controller/AdminController.java
+++ src/main/java/com/example/proyectoCamisetas/controller/AdminController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/admin") 
@@ -27,8 +29,17 @@ public class AdminController {
     @PersistenceContext
     private EntityManager entityManager; 
 
-    @GetMapping({"", "/"}) public String adminHome() { return "admin/index"; }
-    @GetMapping("/camiseta") public String adminCamisetas() { return "redirect:/admin/camiseta/list"; }
+    // Ir al index
+    @GetMapping({"", "/"})
+    public String adminHome() {
+        return "admin/index";
+    }
+
+    // Muestra lista de camisetas
+    @GetMapping("/camiseta")
+    public String adminCamisetas() {
+        return "redirect:/admin/camiseta/list";
+    }
 
     @GetMapping("/camiseta/list") 
     public String listCamisetas(Model model) { 
@@ -37,6 +48,7 @@ public class AdminController {
         return "admin/camiseta/list";
     }
 
+    // Añadimos camiseta
     @GetMapping("/camiseta/add")
     public String addCamisetaForm(Model model) {
         model.addAttribute("camiseta", new Camiseta()); 
@@ -44,6 +56,18 @@ public class AdminController {
         return "admin/camiseta/add";
     }
 
+    @PostMapping({"/camiseta/add"})
+    public String saveCamiseta(@ModelAttribute Camiseta camiseta, @RequestParam(value = "categoriaId", required = false) Integer categoriaId) { 
+        if (categoriaId != null) {
+            Categoria categoriaSeleccionada = categoriaRepository.findById(categoriaId).orElse(null);
+            camiseta.setCategoria(categoriaSeleccionada);
+        }
+
+        camisetaRepository.save(camiseta);
+        return "redirect:/admin/camiseta/list";
+    }
+
+    // Editamos camiseta
     @GetMapping("/camiseta/edit/{id}")
     public String editCamisetaForm(@PathVariable("id") Integer id, Model model) {
         Optional<Camiseta> oCamiseta = camisetaRepository.findById(id);
@@ -56,18 +80,7 @@ public class AdminController {
             return "redirect:/admin/camiseta/list";
     }
 
-    @PostMapping({"/camiseta/save", "/camiseta/add"})
-    public String saveCamiseta(@ModelAttribute Camiseta camiseta, @RequestParam(value = "categoriaId", required = false) Integer categoriaId) { 
-        
-        if (categoriaId != null) {
-            Categoria categoriaSeleccionada = categoriaRepository.findById(categoriaId).orElse(null);
-            camiseta.setCategoria(categoriaSeleccionada);
-        }
-
-        camisetaRepository.save(camiseta);
-        return "redirect:/admin/camiseta/list";
-    }
-
+    // Borramos camiseta
     @GetMapping("/camiseta/del/{id}")
     public String deleteCamisetaForm(@PathVariable("id") Integer id, Model model) {
         Optional<Camiseta> oCamiseta = camisetaRepository.findById(id);
@@ -87,5 +100,29 @@ public class AdminController {
             entityManager.flush(); 
         }
         return "redirect:/admin/camiseta/list";
+    }
+
+    @GetMapping("/maestro_detalle_camisetas")
+    public String maestroDetalleCamisetas(@RequestParam(value = "categoriaId", required = false) Integer categoriaId, Model model) {
+        List<Camiseta> camisetas;
+        Categoria categoriaSeleccionada = null;
+
+        List<Categoria> todasLasCategorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", todasLasCategorias);
+
+        if (categoriaId != null) {
+            Optional<Categoria> oCat = categoriaRepository.findById(categoriaId);
+            if (oCat.isPresent()) {
+                categoriaSeleccionada = oCat.get();
+                camisetas = categoriaSeleccionada.getCamisetas(); 
+            } else 
+                camisetas = camisetaRepository.findAll(); 
+        } else 
+            camisetas = camisetaRepository.findAll();
+        
+        model.addAttribute("camisetas", camisetas);
+        model.addAttribute("categoriaSeleccionada", categoriaSeleccionada);
+
+        return "admin/maestro_detalle_camisetas"; 
     }
 }
